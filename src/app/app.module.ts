158b89b7c391e5cdc0499c586db28159b6b422b9
Debora P <debora.presti@zucchetti.com>
2021-03-30 14:56:25 +0200

    Input path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
--- src/app/jsonedit/jsonedit.component.ts
+++ src/app/jsonedit/jsonedit.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JsoneditComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
