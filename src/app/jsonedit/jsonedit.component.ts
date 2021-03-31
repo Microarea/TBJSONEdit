ee03a1476a4ee735ef59d6b161a3c2e79a1fbaf6
Debora P <debora.presti@zucchetti.com>
2021-03-31 11:28:11 +0200

    Dinamic element in DOM

--- src/app/jsonedit/jsonedit.component.ts
+++ src/app/jsonedit/jsonedit.component.ts
@@ -6,14 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jsonedit.component.css']
 })
 export class JsoneditComponent implements OnInit {
-
-
-
+  public newTitleElem: any;
+  pathJson = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.pathJson = 'test';
+
+    this.newTitleElem = document.getElementById('main-content');
+    this.newTitleElem.innerHTML = '<H3>PROVA AHAHAHAHAHA</H3>';
+    //document.body.classList.add('test xxxxxxxxxxxxxxxxxx');
+    //this.titleContainer.nativeElement.appendChild(this.newTitleElem);
+
   }
 
-
-
+  public onOpenPath() {
+    console.log(this.pathJson);
+  }
 }
